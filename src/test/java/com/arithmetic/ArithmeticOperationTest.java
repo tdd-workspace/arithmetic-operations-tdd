98f0148f770a226c9e2bb5b1f5645ba5c5d81e19
prabanjanjeyasankar <prabanjan@gmail.com>
2024-07-26 17:17:14 +0530

    ADD. Created a first test case and it fails, code : red

--- src/test/java/com/arithmetic/ArithmeticOperationTest.java
+++ src/test/java/com/arithmetic/ArithmeticOperationTest.java
@@ -1,4 +1,13 @@
 package com.arithmetic;
-public class ArithmeticOperationTest {
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ArithmeticOperationTest {
+    @Test
+    void toReturnSixWhenFourIsAddedWithTwo(){
+    ArithmeticOperation arithmeticOperation = new ArithmeticOperationTest();
+    assertEquals(arithmeticOperation.add(4,6));
+}
 }
